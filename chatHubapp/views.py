5eb7a588c6e047dc00b3fc137bb1a9b62566dc52
efesdark <efes.salgir@gmail.com>
2025-05-02 22:36:30 +0300

    App urls added main project urls

--- chatHubapp/views.py
+++ chatHubapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# chathubapp/views.py
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Merhaba, ChatHub'a hoş geldiniz!")
