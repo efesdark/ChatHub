6ed337d0da14fbfb8238f6818d5bff3caefaac2f
efesdark <efes.salgir@gmail.com>
2025-05-03 12:47:37 +0300

    login profile register test added

--- chatHubapp/urls.py
+++ chatHubapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
 
