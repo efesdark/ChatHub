9f5546bc9bfe5d56eb4b6fd44b0756f320b56cd7
efesdark <efes.salgir@gmail.com>
2025-05-03 13:32:37 +0300

    register feature added

--- chatHubapp/views.py
+++ chatHubapp/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect#redirect added
+from .forms import RegisterForm  #import RegisterForm class from created forms.py for user registeration
+from django.contrib import messages
 # Create your views here.
 # chathubapp/views.py
 from django.http import HttpResponse
 
 def test(request):
-    return HttpResponse("Merhaba, ChatHub'a hoş geldiniz!")
+    return HttpResponse("Welcome to ChatHub!")
 
 def home(request):
     return render(request, 'chatHubapp/home.html')
@@ -14,7 +15,15 @@ def login(request):
     return render(request, 'chatHubapp/login.html')
 
 def register(request):
-    return render(request, 'chatHubapp/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created, you can login now')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'chatHubapp/register.html', {'form': form})
 
 def profile(request):
     return render(request, 'chatHubapp/profile.html')
