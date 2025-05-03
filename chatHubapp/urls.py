8fd88f899f8c8cf7232582debe5f5e50819e3802
efesdark <efes.salgir@gmail.com>
2025-05-03 19:06:24 +0300

    Login, log out,profile features added tested and working

--- chatHubapp/urls.py
+++ chatHubapp/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('logout/', views.user_logout, name='logout'),
 ]
 
